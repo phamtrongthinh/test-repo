9f1bfab1d91faaacf0125361c5b95e811ff9a9eb
phamtrongthinh <[200~>
2024-07-30 21:04:30 +0700

    them commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_hoten = new System.Windows.Forms.Label();
+            this.txt_hienthi = new System.Windows.Forms.TextBox();
+            this.btnHienthi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_hoten
+            // 
+            this.lbl_hoten.AutoSize = true;
+            this.lbl_hoten.Location = new System.Drawing.Point(139, 119);
+            this.lbl_hoten.Name = "lbl_hoten";
+            this.lbl_hoten.Size = new System.Drawing.Size(46, 16);
+            this.lbl_hoten.TabIndex = 0;
+            this.lbl_hoten.Text = "Ho ten";
+            // 
+            // txt_hienthi
+            // 
+            this.txt_hienthi.Location = new System.Drawing.Point(249, 116);
+            this.txt_hienthi.Name = "txt_hienthi";
+            this.txt_hienthi.Size = new System.Drawing.Size(434, 22);
+            this.txt_hienthi.TabIndex = 1;
+            // 
+            // btnHienthi
+            // 
+            this.btnHienthi.Location = new System.Drawing.Point(414, 230);
+            this.btnHienthi.Name = "btnHienthi";
+            this.btnHienthi.Size = new System.Drawing.Size(75, 23);
+            this.btnHienthi.TabIndex = 2;
+            this.btnHienthi.Text = "Hien thi";
+            this.btnHienthi.UseVisualStyleBackColor = true;
+            this.btnHienthi.Click += new System.EventHandler(this.btnHienthi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHienthi);
+            this.Controls.Add(this.txt_hienthi);
+            this.Controls.Add(this.lbl_hoten);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_hoten;
+        private System.Windows.Forms.TextBox txt_hienthi;
+        private System.Windows.Forms.Button btnHienthi;
+    }
+}
+
